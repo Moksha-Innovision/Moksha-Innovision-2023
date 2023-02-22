400d1821b78a980b85281572ee52b17808e41c3e
Anit Jha <anitkjha31@gmail.com>
2023-02-22 21:33:59 +0530

    fix: added slight parallaz

--- components/LandingPage/Legacy.tsx
+++ components/LandingPage/Legacy.tsx
@@ -1,12 +1,29 @@
 import Image from "next/image";
 import LegacyCarosel from "./LegacyCarosel";
+import  {Parallax}  from "react-scroll-parallax";
+
 
 const Legacy = () => {
+
+
+
+
   return (
     <div
       draggable={false}
-      className=" relative min-h-[60vh] w-[100vw] bg-black bg-legacy bg-cover  bg-repeat md:h-[80vh] lg:h-[100vh]"
+      className=" relative min-h-[60vh] w-[100vw] bg-black  bg-cover  bg-repeat md:h-[80vh] lg:h-[100vh]"
     >
+      <Parallax speed={0} className='absolute top-0 object-cover h-full w-full contrast-100'>
+
+        <video autoPlay loop muted className="absolute top-0 object-cover h-full w-full contrast-100"
+        >
+          <source src="/output(compress-video-online.com).webm" type="video/webm"></source>
+        </video>
+      <div className=" bg-gradient-to-t from-[rgba(2,3,4,0.5)] via-[rgba(1,2,4,0.3)]  to-[rgba(0,0,0)] backdrop-blur-sm absolute top-0 object-cover h-full w-full pointer-events-none">
+      </div>
+      </Parallax>
+
+     
       <div className="absolute -bottom-[40vh] flex w-full justify-center lg:-bottom-[46vh]">
         <div className="h-[80vh] min-h-[500px] w-[80vw] min-w-[500px] animate-wheel lg:min-h-[700px]   lg:min-w-[700px]">
           <Image
@@ -16,14 +33,17 @@ const Legacy = () => {
             width={100}
             height={100}
             alt={"cross modal"}
-            className="h-full w-full "
+            className="h-full w-full opacity-60"
             quality={1}
           />
         </div>
       </div>
+
+     
       <div className="absolute bottom-0">
         <LegacyCarosel />
       </div>
+      
     </div>
   );
 };
