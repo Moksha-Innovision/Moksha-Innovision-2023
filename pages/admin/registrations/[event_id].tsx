e1d9ae0b34a386f476aa220768614110f47b02b3
deveHk <connect.to.hk@gmail.com>
2023-03-09 05:05:53 +0530

    feature: reg table and xls

--- pages/admin/registrations/[event_id].tsx
+++ pages/admin/registrations/[event_id].tsx
@@ -5,43 +5,44 @@ import {
 } from "@supabase/auth-helpers-react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { FaRedo } from "react-icons/fa";
 import Dashboard from "../../../components/dashboard/ui/Dashboard";
 import ParticipantTable from "../../../components/dashboard/ui/Table/ParticipantTable";
 import Notadmin from "../../../components/FallbackPages/notadmin";
 type Props = {};
 
 const Registrations = (props: Props) => {
+  const tableRef = useRef(null);
   const session = useSession();
   const router = useRouter();
   const supabase = useSupabaseClient();
   const { event_id } = router.query;
-  console.log(event_id);
+  //console.log(event_id);
   const user = useUser();
   const [regsData, setRegsData] = useState<any>("");
 
   const getRegistrations = async () => {
     const { data, error } = await supabase
       .from("event_registerations")
-      .select("*")
-
+      .select("*,profiles(*)")
       // Filters
       .eq("e_id", event_id);
     sessionStorage.setItem(`${event_id}/regs`, JSON.stringify(data));
     setRegsData(data);
-    console.log(regsData);
+    //console.log(regsData);
     console.log(error);
   };
   useEffect(() => {
     let regData = JSON.parse(
       sessionStorage.getItem(`${event_id}/regs`) || "{}"
     );
-    if (Object.keys(regData).length == 0) {
+    if (!regData || Object.keys(regData).length == 0) {
       getRegistrations();
     } else {
       setRegsData(regData);
     }
-    console.log(regData);
+    //console.log(regData);
   }, []);
 
   if (user) {
@@ -67,11 +68,23 @@ const Registrations = (props: Props) => {
           <title>Moksha Innovision&apos;23 | Dashboard</title>
           <meta property="og:title" content="" key="title" />
         </Head>
-        <div className="container m-auto max-h-screen w-[350px] px-2 sm:w-[500px] md:w-full md:px-4 lg:px-4 ">
-          <h1 className="mb-8 mt-6 text-center text-3xl font-semibold drop-shadow-glow md:text-5xl">
-            Participants
+        <div className="container relative m-auto max-h-screen   px-2 sm:w-[500px] md:w-full md:px-4 lg:px-4">
+          <h1 className="mb-8 mt-6 flex justify-center gap-2 text-center text-3xl font-semibold drop-shadow-glow md:text-5xl">
+            <span>Participants</span>
+            <button
+              onClick={getRegistrations}
+              className="flex flex-col items-center justify-center rounded-full  bg-prussian-blue-300 bg-opacity-30 p-2 hover:bg-opacity-40"
+            >
+              <FaRedo className="h-[25px] w-[25px]" />
+              <span className="text-sm font-semibold">Reload</span>
+            </button>
           </h1>
-          <ParticipantTable />
+
+          {regsData ? (
+            <ParticipantTable data={regsData} ref={tableRef} />
+          ) : (
+            "NO DATA"
+          )}
         </div>
       </Dashboard>
     </>
